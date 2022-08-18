a8f1fa40a4605bef033c7956ea42b83158dc6cce
Phangggggggg <16587.mnr@gmail.com>
2022-08-18 17:00:57 +0700

    422 sth error:

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from typing import Union
 from fastapi import FastAPI
 from .MockApi import MockApi
 from .OrderServices import OrderIn,OperatingCost
+from .Auth import Register,Login
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -17,6 +18,8 @@ app.add_middleware(
 app.include_router(OrderIn.router)
 app.include_router(OperatingCost.router)
 app.include_router(MockApi.router)
+app.include_router(Register.router)
+app.include_router(Login.router)
 
 @app.get("/")
 def read_root():
