eb53247fd71d243a040d785d2944ddc110e543fa
Phangggggggg <16587.mnr@gmail.com>
2022-09-12 21:08:22 +0700

    ADD ROLE

--- app/Router/user.py
+++ app/Router/user.py
@@ -1,5 +1,5 @@
 
-from fastapi import APIRouter,Depends,HTTPException,status,Response
+from fastapi import APIRouter,Depends,HTTPException,status
 from app.auth.gen_token import create_access_token
 from app.auth.hashing import Hash
 from app.schemas import UserDisplay,User
@@ -18,7 +18,8 @@ def register(request: User):
     user_doc = {
         'email': request.email,
         'username': request.username,
-        'password': Hash.bcrypt(request.password)
+        'password': Hash.bcrypt(request.password),
+        'role': request.role
     }
     if collection.count_documents({"username":request.username})!=0:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Username is already existed.")
@@ -34,10 +35,10 @@ def login(request: OAuth2PasswordRequestForm = Depends()):
      if not user:
          raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="No User")
      if not Hash.verify(password,request.password):
-         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Incorrect Password")
+         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Incorrect Password")
      access_token = create_access_token(data={'username':request.username})
-     return{
+     return {
          'access_token': access_token,
          'token_type': 'bearer',
-         'user_id': username
+         'username': username
      }
